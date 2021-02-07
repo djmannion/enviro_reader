774a8e74b2e5c137bc4da513ebab6b0e8ea94871
Damien Mannion <d.mannion@unsw.edu.au>
2021-02-07 15:58:10 +1100

    -

--- enviro_reader.py
+++ enviro_reader.py
@@ -24,7 +24,7 @@ import pms5003
 READINGS_PATH = pathlib.Path("~/readings").expanduser()
 DB_PATH = READINGS_PATH / "djm_enviro_readings.db"
 
-assert DB_PATH.exists()
+#assert DB_PATH.exists()
 
 READING = collections.namedtuple(
     "reading",
@@ -105,12 +105,12 @@ def take_reading(reading_number):
             pm_tries += 1
 
     if not pm_ok:
-        pm1_0 = pm2_5 = pm10_0 = float("nan")
+        pm1_0 = pm2_5 = pm10_0 = "NULL"
     else:
         (pm1_0, pm2_5, pm10_0) = [pm.pm_ug_per_m3(n) for n in [1.0, 2.5, 10.0]]
 
     reading = READING(
-        date=datetime.datetime.now().isoformat(),
+        timestamp=datetime.datetime.now().isoformat(),
         reading_number=reading_number,
         temperature=BME280.temperature,
         humidity=BME280.humidity,
